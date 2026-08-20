837fabee37cad8db4a4a1d938ed3261bea9bfe15
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-20 18:15:32 -0300

    Testando o uso de 'else if'

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        boolean permitido_entrar = true;
+        boolean permitido_entrar = false;
         if (permitido_entrar == true) {
             System.out.println("Permitido entrar");
-        }
-        else {
-            System.out.println("Proibido entrar");
+        } else if (permitido_entrar == false) {
+            System.out.println(("Proibido entrar"));
+        } else {
+            System.out.println("Resposta inválida");
         }
 
     }
