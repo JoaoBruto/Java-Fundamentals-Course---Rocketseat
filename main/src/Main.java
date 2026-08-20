48b3a53e174630e0bdbea7e1ef4ebc6ea1cdcd77
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-20 17:42:49 -0300

    Tipos primitivos (inteiros)

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var nome = "João";
+        byte idade = 22;
+        short altura = 156;
+        int clientes = 1564;
+        long pessoas_na_terra = 830954267;
+
     }
 }
