9cc1de8ebe0c27fb7b166144c45612ff0e66ca31
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-20 17:44:55 -0300

    Variável boolean

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        byte idade = 22;
-        short altura = 156;
-        int clientes = 1564;
-        long pessoas_na_terra = 830954267;
+        boolean cadastro_ativo = true;
 
     }
 }
