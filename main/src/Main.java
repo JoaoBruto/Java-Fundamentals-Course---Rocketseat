161afbe9c9b0d1c33e3cad91442b13634f708c5c
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-20 17:55:45 -0300

    Usando a variável if e else

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         double altura = 1.82;
-        float salario = 1420.85f;
+        if(altura > 1.90){
+            System.out.println("Maior");
+        } else {
+            System.out.println("Menor");
+        }
 
     }
 }
