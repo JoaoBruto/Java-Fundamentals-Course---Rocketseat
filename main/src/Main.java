87bb1c02eab08c078e05bd45bcf2e72a832cd376
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-21 14:13:54 -0300

    Primeira forma de declarar arrays

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        boolean permitido_entrar = false;
-        if (permitido_entrar == true) {
-            System.out.println("Permitido entrar");
-        } else if (permitido_entrar == false) {
-            System.out.println(("Proibido entrar"));
-        } else {
-            System.out.println("Resposta inválida");
-        }
+        int[] idades = {2,3,4};
+
 
     }
 }
