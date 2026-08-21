6af6f4ee58c6d64fa346fa7a03881741005f815a
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-21 14:52:23 -0300

    Utilização da função 'ArrayList' para criar listas dinâmicas

---
+++ main/src/estruturas-dados/arraylist/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> idades = new ArrayList<Integer>();
+        idades.add(50);
+        idades.add(30);
+        idades.add(10);
+    }
+}
