c45e6ff5b763787822d3651527bb7424294d1e67
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-23 18:32:46 -0300

    Casting | Explicito

--- main/src/fundamentos/casting/CastingImplicitoExample.java
+++ main/src/fundamentos/casting/CastingImplicitoExample.java
@@ -4,7 +4,7 @@ public class CastingImplicitoExample {
     public static  void main(String[] args) {
         int idade1 = 22;
         double idade2 = idade1;
-        idade1 = idade2;
-        System.out.println(idade2);
+        idade1 = (int) idade2;
+        System.out.println(idade1);
     }
 }
