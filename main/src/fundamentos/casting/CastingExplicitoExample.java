761f2581d3cb52e02ef24b1090b8bb9cb6b85ec9
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-23 18:41:48 -0300

    Casting | Explicito - Exemplo prático da mudança de uma String para char

--- main/src/fundamentos/casting/CastingExplicitoExample.java
+++ main/src/fundamentos/casting/CastingExplicitoExample.java
@@ -2,10 +2,9 @@ import java.util.ArrayList;
 
 public class CastingExplicitoExample {
     public static  void main(String[] args) {
-        int idade1 = 22;
-        double idade2 = idade1;
         char letra = 'A';
-        String nome = String.valueOf(letra);
-        letra = nome.charAt(0);
+        String nome = "João";
+        letra = nome.charAt(1);
+        System.out.println(letra);
     }
 }
