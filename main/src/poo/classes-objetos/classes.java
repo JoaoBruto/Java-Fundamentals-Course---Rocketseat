38a10c2a5cf0131d888562ce729fc25556bc2a46
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-24 14:19:21 -0300

    Exemplo de atributos em objetos static's

--- main/src/poo/classes-objetos/classes.java
+++ main/src/poo/classes-objetos/classes.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 public class classes {
     String nome;
+    static String nome2;
     public static void main(String[] args) {
-
+        System.out.println(nome2);
     }
 
     void declaraNome(){
+        nome2 = "Pedro";
         nome = "João";
     }
     String GetNome() {
-        return nome;
+        return nome2;
     }
 }
