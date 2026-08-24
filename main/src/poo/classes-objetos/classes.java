0c061de884a680658acf2158e24db7f2f7f7cd79
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-24 14:27:22 -0300

    Chamando a função 'teste' diretamente da classe 'classes' sem precisar de uma instância

--- main/src/poo/classes-objetos/classes.java
+++ main/src/poo/classes-objetos/classes.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 
 public class classes {
+
     String nome;
     static String nome2;
     public static void main(String[] args) {
         System.out.println(nome2);
     }
 
+    public static void teste() {
+
+    }
+
     void declaraNome(){
-        nome2 = "Pedro";
+        System.out.println(nome2);
+        classes.teste();
         nome = "João";
     }
     String GetNome() {
-        return nome2;
+        return nome;
     }
 }
