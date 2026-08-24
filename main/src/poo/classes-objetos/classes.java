8ba6de880d6f9b5b577cdda63fd239e5e4f1c715
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-24 14:12:48 -0300

    Exemplo de atributos em objetos

--- main/src/poo/classes-objetos/classes.java
+++ main/src/poo/classes-objetos/classes.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
 
 public class classes {
+    String nome;
     public static void main(String[] args) {
-           
+
+    }
+
+    void declaraNome(){
+        nome = "João";
+    }
+    String GetNome() {
+        return nome;
     }
 }
