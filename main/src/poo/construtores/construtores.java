d683d6614c1eb8184026e7ea9a1ac098f88a973c
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-24 20:01:44 -0300

    Declaração do nome no objeto 'nome'

--- main/src/poo/construtores/construtores.java
+++ main/src/poo/construtores/construtores.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
 public class construtores {
-    
+    String nome;
+    static String nome2;
+
+    public static void main(String[] args) {
+        nome2 = "João";
+        System.out.println(nome2);
+    }
+
+    public construtores(String nome) {
+        this.nome = nome;
+    }
 };
