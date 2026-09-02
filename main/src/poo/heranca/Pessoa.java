7d539a1b4e8e004b66b8ef16c5b5a57401e794ae
Joao Rodrigo <joaofreire185710@gmail.com>
2026-09-02 16:13:28 -0300

    Criação do método de nome

--- main/src/poo/heranca/Pessoa.java
+++ main/src/poo/heranca/Pessoa.java
@@ -1,5 +1,16 @@
 package poo.heranca;
 
+class Ser {
+    String nome;
+    int idade;
+
+    void setNome(String nome) {
+        this.nome = nome;
+        
+
+    };
+}
+
 public class Pessoa {
     void criaMain() {
         Pessoa MeuMain = new Pessoa();
